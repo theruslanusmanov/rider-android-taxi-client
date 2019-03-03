6ecf072f43674404c6da29144d010d753a765be3
owlruslan <ruslanusm6@gmail.com>
2019-03-03 15:08:54 +0500

    Add cardview to ride fragment

--- app/src/main/java/com/github/owlruslan/rider/ui/modes/passanger/ride/RideFragment.kt
+++ app/src/main/java/com/github/owlruslan/rider/ui/modes/passanger/ride/RideFragment.kt
@@ -6,26 +6,38 @@ import android.view.View
 import android.view.ViewGroup
 import com.github.owlruslan.rider.R
 import com.github.owlruslan.rider.di.ActivityScoped
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
 @ActivityScoped
-class RideFragment @Inject constructor() : DaggerFragment(), RideContract.View {
+class RideFragment @Inject constructor() : DaggerFragment(), RideContract.View, OnMapReadyCallback {
 
     @Inject lateinit var presenter: RideContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        presenter.takeView(this);
+        presenter.takeView(this)
     }
 
     override fun onDestroy() {
-        presenter.dropView();  // prevent leaking activity in
+        presenter.dropView()  // prevent leaking activity in
         super.onDestroy()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_passanger_ride, container, false)
+        val view = inflater.inflate(R.layout.fragment_passanger_ride, container, false)
+
+        // Add map to fragment
+        val mapFragment = this.childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this);
+
+        return view
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+
     }
 }
