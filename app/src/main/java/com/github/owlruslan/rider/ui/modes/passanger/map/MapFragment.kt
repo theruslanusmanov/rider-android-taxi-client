3b3e875870bb2f725ccee236c8eabc493ec602e5
owlruslan <ruslanusm6@gmail.com>
2019-03-07 17:05:51 +0500

    Add layouts for search collapsed and expanded

--- app/src/main/java/com/github/owlruslan/rider/ui/modes/passanger/map/MapFragment.kt
+++ app/src/main/java/com/github/owlruslan/rider/ui/modes/passanger/map/MapFragment.kt
@@ -1,20 +1,22 @@
 package com.github.owlruslan.rider.ui.modes.passanger.map
 
 import android.Manifest
+import android.animation.TimeInterpolator
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.transition.Scene
-import android.transition.TransitionManager
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Interpolator
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.transition.*
 import com.github.owlruslan.rider.R
 import com.github.owlruslan.rider.di.ActivityScoped
 import com.github.owlruslan.rider.ui.modes.passanger.search.SearchFragment
@@ -29,8 +31,6 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.android.synthetic.main.activity_main.*
-
 
 @ActivityScoped
 class MapFragment @Inject constructor() : DaggerFragment(), MapContract.View, OnMapReadyCallback {
@@ -53,32 +53,37 @@ class MapFragment @Inject constructor() : DaggerFragment(), MapContract.View, On
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_passanger_map, container, false)
 
-        // Add map to fragment
-        val mapFragment = this.childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
-
+        // Add menu icon
         val drawerLayout = activity!!.findViewById<DrawerLayout>(R.id.drawer_layout)
         val menu = view.findViewById<FloatingActionButton>(R.id.menu)
         menu.setOnClickListener { drawerLayout.openDrawer(GravityCompat.START) }
 
+        // Add map to fragment
+        val mapFragment = this.childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+        // Bottom sheet scene
+        val sceneRoot = view.findViewById(R.id.scene_root) as ViewGroup
+        val sceneCollapsed = Scene.getSceneForLayout(sceneRoot, R.layout.search_collapsed, requireContext())
+        val sceneExpanded = Scene.getSceneForLayout(sceneRoot, R.layout.search_expanded, requireContext())
+        val transition = TransitionSet()
+
+
+
+        // Bottom sheet listeners
         val bottomSheetSearchCardView = view.findViewById<LinearLayout>(R.id.bottomSheetSearch)
         val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetSearchCardView)
         bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
 
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                if (BottomSheetBehavior.STATE_DRAGGING == newState) {
-
-                } else if (BottomSheetBehavior.STATE_COLLAPSED == newState) {
-
-                }
-            }
+            override fun onStateChanged(bottomSheet: View, newState: Int) {}
 
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
-
+                transition.interpolator = Interpolator { slideOffset }
+                transition.duration = 500
+                TransitionManager.go(sceneExpanded, transition)
             }
         })
 
-        // On bottom destination CardView click
         val bottomDestinationCardView = view.findViewById<CardView>(R.id.bottomDestinationCardView)
         bottomDestinationCardView.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
